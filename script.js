e7fb125a17d174a83a0b20f5a9aae6784b5d155b
Arek <arek.stachyra41@gmail.com>
2022-05-25 17:44:54 +0200

    basic numbers display

--- script.js
+++ script.js
@@ -2,21 +2,29 @@ const addBtn = document.querySelector('#add');
 const substractBtn = document.querySelector('#substract');
 const multiplyBtn = document.querySelector('#multiply');
 const divideBtn = document.querySelector('#divive');
-const resultFinal = document.querySelector('#result-final');
-const resultPending = document.querySelector('#result-pending');
+const resultCurrent = document.querySelector('#result-current');
+const resultEquation = document.querySelector('#result-equation');
 const buttonsContainer = document.querySelector('.buttons-container');
-// function operate(operator, num1, num2) {
-// 	switch(operator) {
-// 		case ''
-// 	}
-// }
 
+let nums = [];
+
+
+// HANDLE ALL BUTTONS EVENTS
 buttonsContainer.addEventListener('click', e => {
 	if (e.target.matches('.num')) {
-		resultFinal.textContent = e.target.id;
-	}
+		nums.push(e.target.id);
+		populateDisplay(resultCurrent, nums);
+	} else if (e.target.matches('#del')) {
+		console.log('del');
+	} else if (e.target.matches('#equal')) {
+		console.log('equal');
+	} else if (e.target.matches('#reset')) {
+		console.log('reset');
+	} 
+	// add ifs for equation types
 })
 
+// MATH HANDLING
 function add(num1, num2) {
 	return num1 + num2;
 };
@@ -33,10 +41,25 @@ function divide(num1,num2) {
 	return num1 / num2;
 }
 
-// function sum(nums) {
-// 	return nums.reduce((prev, current) => prev + current,0);
-// };
+function operate(operator, num1, num2) {
+	switch(operator) {
+		case 'add':
+			return add(num1,num2);
+		case 'substract':
+			return substract(num1,num2);
+		case 'multiply':
+			return multiply(num1,num2);
+		case 'divide':
+			return divide(num1,num2);
+		default:
+			console.log('error');
+	}
+}
 
-// function multiply(nums) {
-// 	return nums.reduce((prev, current) => prev * current,1);
-// };
+function populateDisplay(element, content) {
+	if (Array.isArray(content)) {
+		element.textContent = content.join('');
+	} else {
+		element.textContent = content;
+	}
+}
