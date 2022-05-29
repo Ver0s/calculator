668ffa3398d19144916e1f2de69eaf4766236304
Arek <arek.stachyra41@gmail.com>
2022-05-29 10:55:52 +0200

    change event handling

--- script.js
+++ script.js
@@ -1,65 +1,123 @@
-const addBtn = document.querySelector('#add');
-const substractBtn = document.querySelector('#substract');
-const multiplyBtn = document.querySelector('#multiply');
-const divideBtn = document.querySelector('#divive');
-const resultCurrent = document.querySelector('#result-current');
-const resultEquation = document.querySelector('#result-equation');
+const nums = document.querySelectorAll('[data-num]');
+const operators = document.querySelectorAll('[data-operator]');
+const equals = document.querySelector('[data-equals]');
+const reset = document.querySelector('[data-reset]');
+const del = document.querySelector('[data-del]');
 const buttonsContainer = document.querySelector('.buttons-container');
+const resultCurrent = document.querySelector('#result-current')
+const resultEquation = document.querySelector('#result-equation')
 
-let nums = [];
+let currentNum = '';
+let previousNum = '';
+let operator = '';
 
+populateDisplay(resultCurrent, 0);
 
-// HANDLE ALL BUTTONS EVENTS
-buttonsContainer.addEventListener('click', e => {
-	if (e.target.matches('.num')) {
-		nums.push(e.target.id);
-		populateDisplay(resultCurrent, nums);
-	} else if (e.target.matches('#del')) {
-		console.log('del');
-	} else if (e.target.matches('#equal')) {
-		console.log('equal');
-	} else if (e.target.matches('#reset')) {
-		console.log('reset');
-	} 
-	// add ifs for equation types
+// EVENT HANDLING
+
+nums.forEach(num => num.addEventListener('click', (e) => {
+    if (operator === '') {
+        setNum(e, 'currentNum');
+    } else {
+        populateDisplay(resultCurrent, currentNum);
+        setNum(e, 'previousNum');
+    }
+}))
+
+operators.forEach(operator => operator.addEventListener('click', setOperator));
+
+equals.addEventListener('click', () => {
+    result = operate(operator, currentNum, previousNum);
+    populateDisplay(resultCurrent, result);
+    populateDisplay(resultEquation, `${currentNum} ${operator} ${previousNum} = ${result}`);
+    currentNum = result;
+    previousNum = '';
+    operator = '';
 })
 
-// MATH HANDLING
-function add(num1, num2) {
-	return num1 + num2;
-};
+reset.addEventListener('click', resetCalc);
 
-function substract(num1, num2) {
-	return num1 - num2;
-};
+del.addEventListener('click', deleteNum)
 
-function multiply(num1,num2) {
-	return num1 * num2;
+function deleteNum() {
+    currentNum = currentNum.toString().slice(0, -1)
+    populateDisplay(resultCurrent, currentNum);
 }
 
-function divide(num1,num2) {
-	return num1 / num2;
+function resetCalc() {
+    populateDisplay(resultCurrent, 0);
+    populateDisplay(resultEquation, '');
+    currentNum = '';
+    previousNum = '';
+    operator = '';
 }
 
-function operate(operator, num1, num2) {
-	switch(operator) {
-		case 'add':
-			return add(num1,num2);
-		case 'substract':
-			return substract(num1,num2);
-		case 'multiply':
-			return multiply(num1,num2);
-		case 'divide':
-			return divide(num1,num2);
-		default:
-			console.log('error');
-	}
+function setNum(e, num) {
+    if (num === 'currentNum') {
+        currentNum += e.target.textContent;
+        populateDisplay(resultCurrent, currentNum);
+    }
+    if (num === 'previousNum') {
+        previousNum += e.target.textContent;
+        populateDisplay(resultCurrent, previousNum);
+    }
+}
+
+function setOperator(e) {
+    switch (e.target.textContent) {
+        case '+':
+            operator = '+';
+            break;
+        case '-':
+            operator = '-';
+            break;
+        case 'x':
+            operator = 'x';
+            break;
+        case '/':
+            operator = '/';
+            break;
+        case '=':
+            currentNum = operate(operator, currentNum, previousNum);
+            populateDisplay(resultCurrent, currentNum);
+
+    }
 }
 
 function populateDisplay(element, content) {
-	if (Array.isArray(content)) {
-		element.textContent = content.join('');
-	} else {
-		element.textContent = content;
-	}
+    return element.textContent = content;
+}
+
+// MATH HANDLING
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function substract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    currentNum = Number(currentNum);
+    previousNum = Number(previousNum);
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return substract(num1, num2);
+        case 'x':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            console.log('error');
+    }
 }
